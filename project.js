14b3caeef885a861d32ee4c31398ddc66720639e
alexlato <86857302+alexlato@users.noreply.github.com>
2023-06-20 18:30:28 -0700

    getNumberOfLines function

--- project.js
+++ project.js
@@ -21,6 +21,18 @@ const deposit = () => {
   }
 };
 
-const depositAmount = deposit();
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
 
-console.log(depositAmount);
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+const depositAmount = deposit();
+const numberOfLines = getNumberOfLines();
