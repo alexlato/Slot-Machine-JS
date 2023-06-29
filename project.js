b7c5db75816b75aef18561d3ccfca9b7408116ab
alexlato <86857302+alexlato@users.noreply.github.com>
2023-06-28 20:06:49 -0700

    transpose function

--- project.js
+++ project.js
@@ -86,10 +86,33 @@ const spin = () => {
   return reels;
 };
 
-const transpose = (reels) => {};
+const transpose = (reels) => {
+  const rows = [];
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
 
-const reels = spin();
-console.log(reels);
+const printRows = (rows) => {
+  for (const row of rows) {
+    let rowString = "";
+    for (const [i, symbol] of row.entries()) {
+      rowString += symbol;
+      if (i != row.length - 1) {
+        rowString += " | ";
+      }
+    }
+    console.log(rowString);
+  }
+};
+
 let balance = deposit();
 const numberOflines = getNumberOfLines();
 const bet = getBet(balance, numberOflines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
